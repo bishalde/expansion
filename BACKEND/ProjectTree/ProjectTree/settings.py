d93b8f52d56e16f7fcba89bcc9efe4057caaee82
Bishal De <itsbishalde@gmail.com>
2023-09-10 12:42:42 +0530

    fix-6

--- BACKEND/ProjectTree/ProjectTree/settings.py
+++ BACKEND/ProjectTree/ProjectTree/settings.py
@@ -74,13 +74,19 @@ WSGI_APPLICATION = 'ProjectTree.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+   'default': {
+      'ENGINE': 'djongo',
+      'NAME': 'ProjectTree'
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
